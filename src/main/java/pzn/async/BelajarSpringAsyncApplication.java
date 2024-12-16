74b28d937037ecf202c11195162923890bc5ecb1
arfinorestualam <arfino.alam@gmail.com>
2024-12-16 21:24:14 +0700

    enable async

--- src/main/java/pzn/async/BelajarSpringAsyncApplication.java
+++ src/main/java/pzn/async/BelajarSpringAsyncApplication.java
@@ -2,7 +2,10 @@ package pzn.async;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+//to enable async in spring boot, just add this annotation:
+@EnableAsync
 @SpringBootApplication
 public class BelajarSpringAsyncApplication {
 
