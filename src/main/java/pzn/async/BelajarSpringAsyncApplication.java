a17e803dcc09b68025d6304fe588334c0632d7e3
arfinorestualam <arfino.alam@gmail.com>
2024-12-16 21:21:05 +0700

    first

---
+++ src/main/java/pzn/async/BelajarSpringAsyncApplication.java
@@ -0,0 +1,13 @@
+package pzn.async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringAsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BelajarSpringAsyncApplication.class, args);
+    }
+
+}
