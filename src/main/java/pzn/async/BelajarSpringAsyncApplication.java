099ee2beba9987ccb789b4be23083284e5d1a98b
arfinorestualam <arfino.alam@gmail.com>
2024-12-17 22:51:39 +0700

    enable schedule

--- src/main/java/pzn/async/BelajarSpringAsyncApplication.java
+++ src/main/java/pzn/async/BelajarSpringAsyncApplication.java
@@ -3,9 +3,12 @@ package pzn.async;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 //to enable async in spring boot, just add this annotation:
 @EnableAsync
+//to enable scheduler in spring boot, just add this annotation:
+@EnableScheduling
 @SpringBootApplication
 public class BelajarSpringAsyncApplication {
 
