5ffbd3ba17406efa3fcd2490f23835df2213b687
arfinorestualam <arfino.alam@gmail.com>
2024-12-19 15:33:58 +0700

    custom schedule executor

--- src/main/java/pzn/async/AsyncConfiguration.java
+++ src/main/java/pzn/async/AsyncConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 @Configuration
 public class AsyncConfiguration {
@@ -22,4 +23,13 @@ public class AsyncConfiguration {
     public Executor singleTaskExecutor() {
         return Executors.newSingleThreadExecutor();
     }
+
+    //if you want to custom executor for scheduler, first you make @Bean
+    @Bean
+    //using ScheduledExecutorService and the name must include taskScheduler in full name
+    public ScheduledExecutorService taskScheduler() {
+        return Executors.newScheduledThreadPool(10);
+    }
+    //same with custom thread for async, if you make the custom, will be replace
+    //the default setting that set in .properties file
 }
